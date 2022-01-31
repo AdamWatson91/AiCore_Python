082246ed227cdcf805699dd6a132d9a99daeb03c
AdamWatson91 <adamwatson91@gmail.com>
2022-01-31 18:07:24 +0000

    Add first 3 pathway working files

---
+++ VariableAssignment.py
@@ -0,0 +1,25 @@
+# String length
+long_word = 'Pneumonoultramicroscopicsilicovolcanoconiosis'
+print(len(long_word))
+
+first_c = long_word[0]
+last_c = long_word[-1]
+print(first_c)
+print(last_c)
+print(first_c + last_c)
+
+# Basic Arithmetic
+a = 42
+b = 25
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+ab= a+b
+print(type(ab))
+print(type(a+b))
+print(type(a))
+
+#Exercises on Numbers
+print((100*10)+50-5.25)
+print(1 + 2 * 3 - 4 / 6 * (6 + 7)* 8 ** 9)
